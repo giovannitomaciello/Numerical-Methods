ab7c72fb2ec29622ecc195106bb592cfcdabf91c
giovannitomaciello <162450495+giovannitomaciello@users.noreply.github.com>
2024-03-22 10:31:16 +0100

    shake_dimensionato

--- @CINT/shake.m
+++ @CINT/shake.m
@@ -3,7 +3,7 @@ function q = shake(q0,p0,dTdq,dKdp,G,C,m,t)
 [NP, ND] = size(q0);
 NT = numel(t);
 
-ind_constraints = find(C);
+ind_constraints = find(triu(C));
 
 q = zeros(NP,ND,NT);
 p = zeros(NP,ND,NT);
@@ -33,12 +33,14 @@ for i = 3:NT
     q_tilde = 2*q(:,:,i-1) - q(:,:,i-2) - dt^2 * dTdq(q(:,:,i-1))./m;
     q_prec = q(:,:,i-1);
 
-    opt = optimoptions("fsolve","Display","none","OptimalityTolerance",1e-30,...
-        "FunctionTolerance",1e-30);
+    opt = optimoptions("fsolve","Display","none","OptimalityTolerance",1e-18,...
+        "FunctionTolerance",1e-18);
     [unk,iszeros] = fsolve(@(unk) sysEB(unk,NP,q_tilde,q_prec,G,C,m,ind_constraints,dt),unk, opt);
 
     lambda = zeros(NP,NP);
     lambda(ind_constraints) = unk;
+    lambda =lambda+lambda';
+
     [Gq,r2] = G(q_prec,lambda);
 
     q(:,:,i)=q_tilde-dt^2*Gq./m;
@@ -50,6 +52,7 @@ function toZero = sysEB(unk,NP,q_tilde,q_prec,G,C,m,ind_constraints,dt)
              
         lambda = zeros(NP,NP);
         lambda(ind_constraints) = unk;
+        lambda =lambda+lambda';
 
         [Gq,~] = G(q_prec,lambda);
         q = q_tilde-dt^2*Gq./m;
@@ -59,6 +62,6 @@ function toZero = sysEB(unk,NP,q_tilde,q_prec,G,C,m,ind_constraints,dt)
         dz = q(:,3) - q(:,3)';
         r = dx.^2 + dy.^2 + dz.^2; 
        
-        toZero = (r(ind_constraints) - C(ind_constraints).^2)*1e18;
+        toZero = (r(ind_constraints) - C(ind_constraints).^2);
 
 end
